f9403318ee7d6e7f99153e60f614026bc9a78982
florian-bach <you@example.com>
2019-04-15 18:44:50 +0100

    it's almost perfect, for some reason the ordering of the barplots and heatmaps is off, and the frequencies are all 0ss...
    but otherwise fabelprächtig xD

--- R/edger_workflow_64.R
+++ R/edger_workflow_64.R
@@ -23,13 +23,11 @@ files_list <- list.files(path=".", pattern="*.fcs")
 
 flo_set <- read.flowSet(files_list[21:25], transformation = FALSE, truncate_max_range = FALSE)
 
-
-short <- data
-
 # convert to absolute numbers: we're keeping them the same because we still need to check whether edgeR can
 # adequately deal with the lib.size argument differing with the fcs file sizel this is just the smallest number
 # from any of the fcs files
 
+short <- data
 short[,3] <- data[,3]*nrow(flo_set@frames[[files_list[24]]]@exprs)
 short[,4] <- data[,4]*nrow(flo_set@frames[[files_list[24]]]@exprs)
 short[,5] <- data[,5]*nrow(flo_set@frames[[files_list[24]]]@exprs)
@@ -50,15 +48,17 @@ short2[,4] <- data2[,4]*nrow(flo_set@frames[[files_list[24]]]@exprs)
 short2[,5] <- data2[,5]*nrow(flo_set@frames[[files_list[24]]]@exprs)
 short2[,6] <- data2[,6]*nrow(flo_set@frames[[files_list[24]]]@exprs)
 short2[,7] <- data2[,7]*nrow(flo_set@frames[[files_list[24]]]@exprs)
-# combine
+
+# combine, get rid of duplicate rows (cluster and metacluster)
 short <- cbind(short, short2)
 rownames(short) <- short$ClusterID
 short[,c(1:2,8:9)] <-NULL
+
 # make up group matrix, matching "replicates" to each other. also remove _UMAP.fcs suffix
 #groups <- rep(substr(colnames(short)[1:14],1, nchar(colnames(short))-9), times=2)
 groups <- substr(colnames(short), 2,6)
 
-colnames(short) <- substr(colnames(short), 2,6)
+colnames(short) <- groups
 colnames(short)[6:10] <- paste(colnames(short)[1:5], "_1", sep='')
 
 
@@ -81,11 +81,11 @@ fit <- glmQLFit(fit, design, robust=TRUE)
 # topTags(dge_all)
 
 # make a contrast (matrix calculating differential expression between to or more samples) for each person
-base_dod6 <- makeContrasts(groups14c10 - groups14c06,levels=design)
-base_dod <- makeContrasts(groups14c09 - groups14c06,levels=design)
-dod_dod6 <- makeContrasts(groups14c10 - groups14c09,levels=design)
-base_c8 <- makeContrasts(groups14c07 - groups14c06,levels=design)
-base_c10 <- makeContrasts(groups14c08 - groups14c06,levels=design)
+base_dod6 <- makeContrasts(groups14c10 - groups14c06, levels=design)
+base_dod <- makeContrasts(groups14c09 - groups14c06, levels=design)
+dod_dod6 <- makeContrasts(groups14c10 - groups14c09, levels=design)
+base_c8 <- makeContrasts(groups14c07 - groups14c06, levels=design)
+base_c10 <- makeContrasts(groups14c08 - groups14c06, levels=design)
 
 
 # make deg list for each person                                                                             
@@ -103,22 +103,22 @@ deg_base_c8 <- topTags(deg_base_c8, n=64)$table
 deg_base_c10 <- topTags(deg_base_c10, n=64)$table 
 
 
-#add cluser as column 
+#add cluster as column 
 deg_base_dod6$Cluster <- rownames(deg_base_dod6)
 deg_base_dod$Cluster <- rownames(deg_base_dod)
 deg_dod_dod6$Cluster <- rownames(deg_dod_dod6)
 deg_base_c8$Cluster <- rownames(deg_base_c8)
 deg_base_c10$Cluster <- rownames(deg_base_c10)
 
-deg_02 <- deg_02[order(as.numeric(deg_02$Cluster)),]; deg_02$Baseline <- short[,7]; deg_02$Treatment <- short[,1]
-deg_03 <- deg_03[order(as.numeric(deg_03$Cluster)),]; deg_03$Baseline <- short[,8]; deg_03$Treatment <- short[,2]
-deg_05 <- deg_05[order(as.numeric(deg_05$Cluster)),]; deg_05$Baseline <- short[,9]; deg_05$Treatment <- short[,3]
-deg_06 <- deg_06[order(as.numeric(deg_06$Cluster)),]; deg_06$Baseline <- short[,10]; deg_06$Treatment <- short[,4]
-deg_07 <- deg_07[order(as.numeric(deg_07$Cluster)),]; deg_07$Baseline <- short[,11]; deg_07$Treatment <- short[,5]
-deg_09 <- deg_09[order(as.numeric(deg_09$Cluster)),]; deg_09$Baseline <- short[,12]; deg_09$Treatment <- short[,6]
+# deg_02 <- deg_02[order(as.numeric(deg_02$Cluster)),]; deg_02$Baseline <- short[,7]; deg_02$Treatment <- short[,1]
+# deg_03 <- deg_03[order(as.numeric(deg_03$Cluster)),]; deg_03$Baseline <- short[,8]; deg_03$Treatment <- short[,2]
+# deg_05 <- deg_05[order(as.numeric(deg_05$Cluster)),]; deg_05$Baseline <- short[,9]; deg_05$Treatment <- short[,3]
+# deg_06 <- deg_06[order(as.numeric(deg_06$Cluster)),]; deg_06$Baseline <- short[,10]; deg_06$Treatment <- short[,4]
+# deg_07 <- deg_07[order(as.numeric(deg_07$Cluster)),]; deg_07$Baseline <- short[,11]; deg_07$Treatment <- short[,5]
+# deg_09 <- deg_09[order(as.numeric(deg_09$Cluster)),]; deg_09$Baseline <- short[,12]; deg_09$Treatment <- short[,6]
 
 #mark stuff that never surpasses 1% frequency
-deg_02$matters <- ifelse(deg_02$Baseline < 0.01*number_of_cells, ifelse(deg_02$Treatment < 0.01*number_of_cells, "matters_not", "matters"), "matters")
+#deg_02$matters <- ifelse(deg_02$Baseline < 0.01*number_of_cells, ifelse(deg_02$Treatment < 0.01*number_of_cells, "matters_not", "matters"), "matters")
 
 
 deg_base_dod6 <- deg_base_dod6[order(as.numeric(deg_base_dod6$Cluster)),]
@@ -146,6 +146,16 @@ deg_dod_dod6$Timepoint <- "deg_dod_dod6"
 deg_base_c8$Timepoint <- "deg_base_c8"
 deg_base_c10$Timepoint <- "deg_base_c10"
 
+all_degs <- list(deg_base_dod6,deg_base_dod,deg_dod_dod6,deg_base_c8,deg_base_c10)
+
+# make a list of lists that contain re-transformed fold change values
+fold_change  <- lapply(all_degs, function(x){x$Fold_Change <- 2^x$logFC})
+
+# add list to list of dataframes as new column; the simplify argument prevents conversion of every column
+# to a list
+
+all_degs <- mapply(cbind, all_degs, "Fold_Change"= fold_change, SIMPLIFY=F)
+
 
 # deg_02$Cluster <- rownames(deg_02)
 # deg_03$Cluster <- rownames(deg_03)
@@ -155,7 +165,11 @@ deg_base_c10$Timepoint <- "deg_base_c10"
 # deg_09$Cluster <- rownames(deg_09)
 
 # combine them in one big file
-individual_from_all <- rbind(deg_base_dod6, deg_base_dod, deg_dod_dod6, deg_base_c8, deg_base_c10)
+#individual_from_all <- rbind(deg_base_dod6, deg_base_dod, deg_dod_dod6, deg_base_c8, deg_base_c10)
+
+individual_from_all <- ldply(all_degs, rbind)
+
+individual_from_all <- dplyr::filter(individual_from_all, FDR<0.01)
 
 # subset dataframe so only fold changes over 2 and less than 0.5 are included
 upper_cut_off <- dplyr::filter(individual_from_all, logFC > 1)
@@ -168,6 +182,23 @@ nrow(cut_off)
 
 list_of_degs <- split(cut_off, cut_off$Timepoint)
 
+# store what's clusters matter for what timepoint in variables
+clusters_base_c8 <- cut_off %>%
+  dplyr::filter(cut_off$Timepoint=="deg_base_c8") %>%
+  select(Cluster, Fold_Change)
+
+clusters_base_dod <- clusters_base_c8 <- cut_off %>%
+  dplyr::filter(cut_off$Timepoint=="deg_base_dod") %>%
+  select(Cluster, Fold_Change)
+
+clusters_base_dod6 <- clusters_base_c8 <- cut_off %>%
+  dplyr::filter(cut_off$Timepoint=="deg_base_dod6") %>%
+  select(Cluster, Fold_Change)
+
+clusters_dod_dod6 <- clusters_base_c8 <- cut_off %>%
+  dplyr::filter(cut_off$Timepoint=="deg_dod_dod6") %>%
+  select(Cluster, Fold_Change)
+#list_of_degs <- all_degs
 
 
 ######        the plan is to make figures showing a starplot of all their deg clusters with the fold change
@@ -178,40 +209,43 @@ setwd("/Users/s1249052/PhD/flow_data/vac69a/t_cells_only/FlowSOM_big_timecourse_
 
 deg_medians_aggregate  <- read.csv("aggregate_cluster_medians.csv")
 
+
 # super convoluted way of doing it but it works: restrict cluster medians to clusters that can be found in deg list with cutoff of log2 1 and -1
-deg_medians_02 <- deg_medians_aggregate %>%
-  dplyr::filter(ClusterID %in% as.numeric(list_of_degs[[1]]$Cluster)) %>%
-  mutate(Comparison = list_of_degs[[1]]$Timepoint) %>%
-  mutate(FC = paste("Cluster_", ClusterID, " (", substr(as.character(2^as.numeric(list_of_degs[[1]]$logFC)),0,4), ")", sep=''))
+medians_base_c8 <- deg_medians_aggregate %>%
+  dplyr::filter(ClusterID %in% as.numeric(clusters_base_c8$Cluster))%>%
+  mutate(Comparison = "base_c8") %>%
+  mutate(Fold_Change = clusters_base_c8$Fold_Change)
 
-deg_medians_03 <- deg_medians_aggregate %>%
-  dplyr::filter(ClusterID %in% as.numeric(list_of_degs[[2]]$Cluster))  %>%
-  mutate(Comparison = list_of_degs[[2]]$Timepoint) %>%
-  mutate(FC = paste("Cluster_", ClusterID, " (", substr(as.character(2^as.numeric(list_of_degs[[2]]$logFC)),0,4), ")", sep=''))
+medians_base_dod <- deg_medians_aggregate %>%
+  dplyr::filter(ClusterID %in% as.numeric(clusters_base_dod$Cluster))  %>%
+  mutate(Comparison = "base_dod") %>%
+  mutate(Fold_Change = clusters_base_dod$Fold_Change)
 
-deg_medians_05 <- deg_medians_aggregate %>%
-  dplyr::filter(ClusterID %in% as.numeric(list_of_degs[[3]]$Cluster))  %>%
-  mutate(Comparison = list_of_degs[[3]]$Timepoint) %>%
-  mutate(FC = paste("Cluster_", ClusterID, " (", substr(as.character(2^as.numeric(list_of_degs[[3]]$logFC)),0,4), ")", sep=''))
+medians_base_dod6 <- deg_medians_aggregate %>%
+  dplyr::filter(ClusterID %in% as.numeric(clusters_base_dod6$Cluster))  %>%
+  mutate(Comparison = "base_dod6") %>%
+  mutate(Fold_Change = clusters_base_dod6$Fold_Change)
 
-deg_medians_06 <- deg_medians_aggregate %>%
-  dplyr::filter(ClusterID %in% as.numeric(list_of_degs[[4]]$Cluster))  %>%
-  mutate(Comparison = list_of_degs[[4]]$Timepoint) %>%
-  mutate(FC = paste("Cluster_", ClusterID, " (", substr(as.character(2^as.numeric(list_of_degs[[4]]$logFC)),0,4), ")", sep=''))
+medians_dod_dod6 <- deg_medians_aggregate %>%
+  dplyr::filter(ClusterID %in% as.numeric(clusters_dod_dod6$Cluster))  %>%
+  mutate(Comparison = "dod_dod6") %>%
+  mutate(Fold_Change = clusters_dod_dod6$Fold_Change)
 
 
 #put it all together to make ggplots; drop UMAP channels
-deg_medians_aggregate <- rbind(deg_medians_02, deg_medians_03, deg_medians_05, deg_medians_06)
+deg_medians_aggregate <- rbind(medians_base_c8, medians_base_dod, medians_base_dod6, medians_dod_dod6)
 
-deg_medians_all <- select(deg_medians_aggregate, colnames(deg_medians_aggregate)[c(1, 2, 5, 16:17, 25:59, 65, 67, 72)])
+deg_medians_all <- select(deg_medians_aggregate, colnames(deg_medians_aggregate)[c(1, 2, 5, 16:17, 25:59, 65, 67, 72, 73)])
 
 
 colnames(deg_medians_all)[4:42] <- substr(colnames(deg_medians_all)[4:42], 8, nchar(colnames(deg_medians_all)[4:42])-10)
 colnames(deg_medians_all)[3] <- "CD45"
+
+
 # convert to long format
 deg_medians_all$MetaclusterID <- NULL
 long_deg_medians_all <- gather(deg_medians_all, Marker, Intensity, colnames(deg_medians_all)[2:41])
-
+long_deg_medians_all$Direction <- ifelse(long_deg_medians_all$Fold_Change>1, "up", "down")
 # make beautiful iris color palette
 
 #my_palette <- colorRampPalette(c("#ffd4c4", "#ffb5e2", "#ce70e8", "#a347e5", "#6a39ff"))(n=20)
@@ -271,11 +305,7 @@ for(i in unique(long_deg_medians_all$Comparison)){
   # 
   sub_set <- dplyr::filter(long_deg_medians_all, Comparison == i)
   
-  specific_levels <- sub_set %>% 
-    dplyr::filter(Marker == "CD4") %>%
-    arrange(desc(Intensity))
-  
-  specific_levels <- c(as.character(specific_levels$ClusterID))
+  specific_levels <- unique(sub_set[order(sub_set$Fold_Change, decreasing = TRUE),"ClusterID"][c(seq(from=1, to=nrow(sub_set), by=2))])
   
   assign(paste("comparison_", unique(sub_set$Comparison), sep=''),
          ggplot(data = sub_set, aes_(x = factor(sub_set$ClusterID, levels = specific_levels), y = factor(sub_set$Marker, levels = rev(marker_levels)), group=sub_set$Comparison))+
@@ -283,7 +313,8 @@ for(i in unique(long_deg_medians_all$Comparison)){
            scale_fill_gradientn(colors=rev(my_palette))+
            scale_y_discrete(position = "left")+
            xlab(NULL)+
-           ggtitle(paste("Volunteer ", i,sep=''))+
+           facet_grid(~ Direction, scales = "free")+
+           ggtitle(paste(i))+
            theme(panel.border = element_blank(),
                  axis.text.y.left = element_text(size=35),
                  axis.line.y.left = element_blank(),
@@ -298,61 +329,69 @@ for(i in unique(long_deg_medians_all$Comparison)){
                  legend.title = element_blank(),
                  legend.position = "none",
                  plot.title = element_text(size = 45, hjust = 0.5),
-                 plot.margin = unit(c(1,0,1,0), "cm"))
+                 plot.margin = unit(c(1,0,1,0), "cm"),
+                 strip.text.x = element_text(size=28))
   )
+print(head(sub_set))
 } 
 
 
 
-ggsave("sandbox.pdf", grid.arrange(comparison_deg_base_c8, comparison_deg_base_dod, comparison_deg_base_dod6, comparison_deg_dod_dod6, ncol=3, nrow=2, layout_matrix = rbind(c(1,2,3),
+ggsave("sandbox.pdf", grid.arrange(comparison_base_c8, comparison_base_dod, comparison_base_dod6, comparison_dod_dod6, ncol=3, nrow=2, layout_matrix = rbind(c(1,2,3),
                                                                                                                                                              c(4,5,6))
 ),  width = 40, height = 40, limitsize = F)
 
 #######         figures for cluster abundances
 
 data <- read.csv("/Users/s1249052/PhD/flow_data/vac69a/t_cells_only/FlowSOM_big_timecourse_03_(copy)_(copy)_results/results/cluster_abundances.csv")
-short <- select(data, colnames(data[3:14]))
+short <- select(data, colnames(data[3:7]))
 
 
-clusters_base_c8 <- as.integer(list_of_degs[[1]]$Cluster)
-clusters_base_dod <- as.integer(list_of_degs[[2]]$Cluster)
-clusters_base_dod6 <- as.integer(list_of_degs[[3]]$Cluster)
-clusters_dod_dod6 <- as.integer(list_of_degs[[4]]$Cluster)
 
-
-abun_clusters_02 <- short[c(clusters_base_c8),c(2,1)]
-abun_clusters_02$ClusterID <- as.factor(clusters_base_c8)
+abun_clusters_02 <- short[c(as.numeric(clusters_base_c8$Cluster)),c(2,1)]
+abun_clusters_02$ClusterID <- rownames(abun_clusters_02)
 abun_clusters_02$Comparison <- "base_c8"
-colnames(abun_clusters_02) <- c("post", "pre", "ClusterID", "Comparison")
+abun_clusters_02$Fold_Change <- clusters_base_c8$Fold_Change
+colnames(abun_clusters_02)[1:2] <- c("post", "pre")
 
-abun_clusters_03 <- short[c(clusters_base_dod),c(4,1)]
-abun_clusters_03$ClusterID <- as.factor(clusters_base_dod)
+abun_clusters_03 <- short[c(as.numeric(clusters_base_dod$Cluster)),c(4,1)]
+abun_clusters_03$ClusterID <- rownames(abun_clusters_03)
 abun_clusters_03$Comparison <- "base_dod"
-colnames(abun_clusters_03) <- c("post", "pre", "ClusterID", "Comparison")
+abun_clusters_03$Fold_Change <- clusters_base_dod$Fold_Change
+colnames(abun_clusters_03)[1:2] <- c("post", "pre")
 
-abun_clusters_05 <- short[c(clusters_base_dod6),c(5,1)]
-abun_clusters_05$ClusterID <- as.factor(clusters_base_dod6)
+abun_clusters_05 <- short[c(as.numeric(clusters_base_dod6$Cluster)),c(5,1)]
+abun_clusters_05$ClusterID <- rownames(abun_clusters_05)
 abun_clusters_05$Comparison <- "base_dod6"
-colnames(abun_clusters_05) <- c("post", "pre", "ClusterID", "Comparison")
+abun_clusters_05$Fold_Change <- clusters_base_dod6$Fold_Change
+colnames(abun_clusters_05)[1:2] <- c("post", "pre")
 
-abun_clusters_06 <- short[c(clusters_dod_dod6),c(5,4)]
-abun_clusters_06$ClusterID <- as.factor(clusters_dod_dod6)
+abun_clusters_06 <- short[c(as.numeric(clusters_dod_dod6$Cluster)),c(5,4)]
+abun_clusters_06$ClusterID <- rownames(abun_clusters_06)
 abun_clusters_06$Comparison <- "dod_dod6"
-colnames(abun_clusters_06) <- c("post", "pre", "ClusterID", "Comparison")
+abun_clusters_06$Fold_Change <- clusters_dod_dod6$Fold_Change
+colnames(abun_clusters_06)[1:2] <- c("post", "pre")
 
 
 abun_clusters <- rbind(abun_clusters_02, abun_clusters_03, abun_clusters_05, abun_clusters_06)
 
 long_abun_clusters <- gather(abun_clusters, Timepoint, Count, c("pre", "post"))
-long_abun_clusters$Count <- long_abun_clusters$Count/nrow(flo_set@frames[[files_list[24]]]@exprs)
+long_abun_clusters$Frequency <- long_abun_clusters$Count/nrow(flo_set@frames[[files_list[24]]]@exprs)
 
 
 for(i in unique(long_abun_clusters$Comparison)){
+  
+  sub_set <- dplyr::filter(long_abun_clusters, Comparison == i)
+  
+  specific_levels <- sub_set[order(sub_set$Fold_Change, decreasing = TRUE),"ClusterID"][c(seq(from=1, to=nrow(sub_set), by=2))]
+  
   assign(paste(i,"_bar", sep=''), 
-    
-  ggplot(data = dplyr::filter(long_abun_clusters, Comparison == i),
-         aes(x=factor(ClusterID), y=Count, fill=factor(Timepoint, levels=c("pre", "post")))
-         )+
+    ggplot(data = sub_set,
+         aes(x = factor(ClusterID, levels=specific_levels),
+              y = Frequency,
+              fill = factor(Timepoint, levels=c("pre", "post"))
+              )
+             )+
   geom_bar(stat="identity", position=position_dodge())+
   scale_fill_brewer(palette="Paired")+
   xlab("Cluster ID")+
@@ -369,11 +408,59 @@ for(i in unique(long_abun_clusters$Comparison)){
         axis.text.y = element_text(size=20, color="black")))
 }
 
-ggsave("heatmap_plus_abundance_base_c8.pdf", grid.arrange(comparison_deg_base_c8, base_c8_bar, layout_matrix = rbind(c(1,1,NA),c(1,1,2),c(1,1,NA))), height = 20, width=28)
-ggsave("heatmap_plus_abundance_base_dod.pdf", grid.arrange(comparison_deg_base_dod, base_dod_bar, layout_matrix = rbind(c(1,1,NA),c(1,1,2),c(1,1,NA))), height = 20, width=28)
-ggsave("heatmap_plus_abundance_base_dod6.pdf", grid.arrange(comparison_deg_base_dod6, base_dod6_bar, layout_matrix = rbind(c(1,1,NA),c(1,1,2),c(1,1,NA))), height = 20, width=28)
-ggsave("heatmap_plus_abundance_dod_dod6.pdf", grid.arrange(comparison_deg_dod_dod6, dod_dod6_bar, layout_matrix = rbind(c(1,1,NA),c(1,1,2),c(1,1,NA))), height = 20, width=28)
+ggsave("heatmap_plus_abundance_base_c8.pdf", grid.arrange(comparison_base_c8, base_c8_bar, layout_matrix = rbind(c(1,1,NA),c(1,1,2),c(1,1,NA))), height = 20, width=28)
+ggsave("heatmap_plus_abundance_base_dod.pdf", grid.arrange(comparison_base_dod, base_dod_bar, layout_matrix = rbind(c(1,1,NA),c(1,1,2),c(1,1,NA))), height = 20, width=28)
+ggsave("heatmap_plus_abundance_base_dod6.pdf", grid.arrange(comparison_base_dod6, base_dod6_bar, layout_matrix = rbind(c(1,1,NA),c(1,1,2),c(1,1,NA))), height = 20, width=28)
+ggsave("heatmap_plus_abundance_dod_dod6.pdf", grid.arrange(comparison_dod_dod6, dod_dod6_bar, layout_matrix = rbind(c(1,1,NA),c(1,1,2),c(1,1,NA))), height = 20, width=28)
+
+
+
+
+
+
+# > specific_levels
+# [1] "38" "61" "37" "29" "51" "46" "23" "28" "30" "57" "33" "19" "26" "10" "2"  "11" "18" "3" 
+
 
 
 
 ########   combine the two for loops so the order f the bar graph matches the drapes
+
+
+long_individual_from_all <- gather(individual_from_all, Cluster, Fold_Change, colnames(individual_from_all)[6:10] )
+
+
+ggplot(data = dplyr::filter(individual_from_all, Timepoint=="deg_base_dod6"), aes(x=factor(Cluster), y=Fold_Change, fill=factor(Timepoint)))+
+  geom_bar(stat="identity", position=position_dodge())+
+  scale_fill_brewer(palette="Paired")+
+  xlab("Cluster ID")+
+  scale_y_log10()+
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1))+
+  theme(legend.title = element_blank(),
+        legend.text = element_text(size = 20),
+        legend.position = "top", 
+        legend.justification = "center",
+        legend.direction = "horizontal",
+        axis.line = element_line(colour = "black"),
+        axis.text.x = element_text(size=20, color="black"),
+        axis.title.x = element_text(size=24, color="black"),
+        axis.title.y = element_text(size=24, color="black"),
+        axis.text.y = element_text(size=20, color="black"))
+
+
+ggplot(data = long_abun_clusters, aes(x=factor(ClusterID), y=Count, group=Timepoint, fill=Comparison))+
+  geom_bar(stat="identity", position=position_dodge())+
+  scale_fill_brewer(palette="Paired")+
+  xlab("Cluster ID")+
+  scale_y_log10()+
+  #scale_y_continuous(labels = scales::percent_format(accuracy = 1))+
+  theme(legend.title = element_blank(),
+        legend.text = element_text(size = 20),
+        legend.position = "top", 
+        legend.justification = "center",
+        legend.direction = "horizontal",
+        axis.line = element_line(colour = "black"),
+        axis.text.x = element_text(size=20, color="black"),
+        axis.title.x = element_text(size=24, color="black"),
+        axis.title.y = element_text(size=24, color="black"),
+        axis.text.y = element_text(size=20, color="black"))
