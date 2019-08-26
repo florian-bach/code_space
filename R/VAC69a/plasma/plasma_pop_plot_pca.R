e355f9cf72d352e0d677f898acc7c02778bdecf8
florian-bach <you@example.com>
2019-08-26 18:33:10 +0100

    check out those SPLINES yO

--- R/VAC69a/plasma/plasma_pop_plot_pca.R
+++ R/VAC69a/plasma/plasma_pop_plot_pca.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(ggbiplot)
-
+library(rgl)
 
 data <- read.csv("/Users/s1249052/PhD/plasma/vac69a/vac69a_Vivax_plasma_analytes_no_inequalities.csv")
 data<- na.omit(data)
@@ -27,16 +27,127 @@ ggplot(long_data, aes(x=factor(long_data$timepoint, levels=c("C-1", "DoD", "T+6"
 
 ####     pca plot
 
-data <- spread(long_data, Analyte, Concentration)
+data2 <- spread(long_data, Analyte, Concentration)
 
 
-plasma_pca <- prcomp(data[,3:37], center = T, scale. = T)
+plasma_pca <- prcomp(data2[,3:37], center = T, scale. = T)
 
 summary(plasma_pca)  
 
-pca <- ggbiplot(plasma_pca, choices=2:3, obs.scale = 1, var.scale = 1,var.axes=FALSE, groups = data$Volunteer)
+pca12 <- ggbiplot(plasma_pca, circle = T, choices=1:2, obs.scale = 1, var.scale = 1,var.axes=FALSE, groups = data$Volunteer, labels = data$timepoint)+
+  theme_minimal()+
 
-plotly::ggplotly(pca+theme(legend.title = element_blank()),tiptool="timepoint")
+pca12
 
-timepoint
 
+pca23 <- ggbiplot(plasma_pca, choices=2:3, obs.scale = 1, var.scale = 1,var.axes=FALSE, groups = data$Volunteer, labels = data$timepoint)+
+  theme(legend.title = element_blank())
+pca23
+#plotly::ggplotly(pca, tooltip="timepoint")
+
+rgl::plot3d(plasma_pca$x[,1], plasma_pca$x[,2], plasma_pca$x[,3], col=as.numeric(as.factor(data$timepoint)))
+
+my_paired_palette <- c("#FB9A99","#E31A1C","#A6CEE3", "#1F78B4", "#B2DF8A", "#33A02C")
+
+
+#####     handmade
+
+ggplot()+
+  # geom_point(aes(x=plasma_pca$x[,1], y=plasma_pca$x[,2], color=data$Volunteer), size=4)+
+  # ggrepel::geom_label_repel(aes(x=plasma_pca$x[,1], y=plasma_pca$x[,2], color=data$Volunteer, label=data$timepoint), point.padding = 0.2, size=4)+
+  geom_text(aes(x=plasma_pca$x[,1], y=plasma_pca$x[,2], color=data$Volunteer, label=data$timepoint), size=5, fontface="bold")+
+  # geom_label(aes(x=plasma_pca$x[,1], y=plasma_pca$x[,2], color=data$Volunteer, label=data$timepoint), size=4)+
+  xlab(paste("PC1 ", summary(plasma_pca)$importance[2,1]*100, "%", sep = ""))+
+  ylab(paste("PC2 ", summary(plasma_pca)$importance[2,2]*100, "%", sep = ""))+
+  scale_shape_manual(values=c(16:19))+
+  scale_color_manual(values=my_paired_palette)+
+  theme_minimal()+
+  theme(legend.title = element_blank(),
+        axis.text = element_text(size=14),
+        axis.title = element_text(size=17))
+   
+###########
+
+
+
+
+#cytof test
+
+
+cytof <- read.csv("/Users/s1249052/PhD/cytof/better_gating/big_flowsoms/FlowSOM_all_cd4s_baseline_dod_t6_results/results/cluster_abundances.csv")
+colnames(cytof)[3:20] <- paste(rep(c("V06", "V07", "V09", "V02", "V03", "V05"), each=3), rep(c("Baseline_01", "DoD_01", "DoD+6_01"), times=6), sep='_')
+
+long_cytof <- gather(cytof, Dataset, Frequency, colnames(cytof[3:20]))
+
+long_cytof$Volunteer <- substr(long_cytof$Datase, 1, 3)
+long_cytof$Timepoint <- substr(long_cytof$Dataset, 5, nchar(long_cytof$Dataset)-3)
+long_cytof$ClusterID <- paste("C_", long_cytof$ClusterID, sep='')
+long_cytof <- select(long_cytof, -MetaclusterID, -Dataset)
+
+broad_cytof <- spread(long_cytof, ClusterID, Frequency)
+
+cytof_pca <- prcomp(broad_cytof[,3:102], center = T, scale. = T)
+
+
+
+
+
+
+
+
+
+
+cytof <- read.csv("/Users/s1249052/PhD/cytof/vac63c/analysis/FlowSOM_all_cd4+_2_results/results/cluster_abundances.csv")
+
+colnames(cytof)[3:49] <- substr(colnames(cytof)[3:49], nchar(colnames(cytof)[3:49])-10, nchar(colnames(cytof)[3:49])-4)
+
+# get rid of control files
+cytof <- cytof[,-grep("tr", colnames(cytof), fixed = T)]
+colnames(cytof) <- gsub("_", "", colnames(cytof), fixed=T)
+colnames(cytof) <- gsub(".", "", colnames(cytof), fixed=T)
+
+
+long_cytof <- gather(cytof, Dataset, Frequency, colnames(cytof[3:46]))
+
+long_cytof$Volunteer <- substr(long_cytof$Datase, 1, 3)
+long_cytof$Timepoint <- substr(long_cytof$Dataset, 4, nchar(long_cytof$Dataset))
+long_cytof$ClusterID <- paste("C_", long_cytof$ClusterID, sep='')
+long_cytof <- select(long_cytof, -MetaclusterID, -Dataset)
+
+broad_cytof <- spread(long_cytof, ClusterID, Frequency)
+
+cytof_pca <- prcomp(broad_cytof[,3:102], center = T, scale. = T)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ggplot()+
+   # geom_point(aes(x=cytof_pca$x[,1], y=cytof_pca$x[,2]), size=4)+
+  # ggrepel::geom_label_repel(aes(x=cytof_pca$x[,1], y=cytof_pca$x[,2], color=data$Volunteer, label=data$timepoint), point.padding = 0.2, size=4)+
+  #geom_text(aes(x=cytof_pca$x[,1], y=cytof_pca$x[,2], color=broad_cytof$Volunteer, label=broad_cytof$Timepoint), size=5, fontface="bold")+
+  geom_line(aes(x=cytof_pca$x[,1], y=cytof_pca$x[,2], group=broad_cytof$Volunteer, color=broad_cytof$Volunteer))+
+  # geom_label(aes(x=cytof_pca$x[,1], y=cytof_pca$x[,2], color=data$Volunteer, label=data$timepoint), size=4)+
+  xlab(paste("PC1 ", summary(cytof_pca)$importance[2,1]*100, "%", sep = ""))+
+  ylab(paste("PC2 ", summary(cytof_pca)$importance[2,2]*100, "%", sep = ""))+
+  scale_shape_manual(values=c(16:19))+
+  #scale_color_manual(values=my_paired_palette)+
+  theme_minimal()+
+  theme(legend.title = element_blank(),
+        axis.text = element_text(size=14),
+        axis.title = element_text(size=17))
+
+
+
+rgl::plot3d(cytof_pca$x[,1], cytof_pca$x[,2], cytof_pca$x[,3], col=as.numeric(as.factor(broad_cytof$Volunteer)))
---
+++ R/misc/SPLINES_yo.R
@@ -0,0 +1,48 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+# The following initializes usage of Bioc devel
+BiocManager::install(version='devel')
+
+BiocManager::install("fission")
+
+
+library(DESeq2)
+library("fission")
+data("fission")
+
+se <- fission
+
+ddsTC <- DESeqDataSet(fission, ~ strain + minute + strain:minute)
+
+ddsTC <- DESeq(ddsTC, test="LRT", reduced = ~ strain + minute)
+resTC <- results(ddsTC)
+resTC$symbol <- mcols(ddsTC)$symbol
+head(resTC[order(resTC$padj),], 4)
+
+fiss <- plotCounts(ddsTC, which.min(resTC$padj), 
+                   intgroup = c("minute","strain"), returnData = TRUE)
+fiss$minute <- as.numeric(as.character(fiss$minute))
+
+yeast1 <- data.frame(spline(x=filter(fiss$strain=="wt")$minute, y=fiss$count, n=100))
+yeast2 <- data.frame(spline(x=fiss$minute, y=fiss$count, n=100))
+
+yeasts <- list(yeast1, yeast2)
+
+
+yeet <- split(fiss, fiss$strain)
+
+applied_yeet <- lapply(yeet, FUN = function(x){spline(x$minute, x$count, n=100)})  
+
+
+ggplot()+
+  geom_point(data=fiss, aes(x = minute, y = count, color = strain, group = strain))+
+  #scale_y_log10()+
+  #geom_line(data=hello, aes(x=x, y=y))
+  #geom_line(data=dplyr::bind_rows(yeasts, .id="df"), aes(x=x, y=y, color=df))
+  geom_line(data=dplyr::bind_rows(applied_yeet, .id="df"), aes(x=x, y=y, color=df))
+  
+
+
+
+hello1 <- data.frame(spline(x=fiss$minute, y=fiss$count))
