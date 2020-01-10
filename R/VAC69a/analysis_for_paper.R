f1e7fbc5cd72344b7848b02ff174a0b613b12f18
florian-bach <you@example.com>
2020-01-10 19:32:27 +0000

    yaaaay

--- R/VAC69a/analysis_for_paper.R
+++ R/VAC69a/analysis_for_paper.R
@@ -9,7 +9,13 @@ library(gridExtra)
 library(diffcyt)
 
 `%!in%` = Negate(`%in%`)
-start_time <- Sys.time()
+
+myPalette <- colorRampPalette(rev(brewer.pal(11, "Spectral")))
+red_palette <- c(myPalette(100), rep(myPalette(100)[100], 200))
+
+sc <- scale_colour_gradientn(colours = red_palette, limits=c(0, 8))
+
+#start_time <- Sys.time()
 ###
 
 
@@ -17,7 +23,7 @@ start_time <- Sys.time()
 # CATALYST
 
 
-### read in metadata
+### read in metadata etc. ####
 setwd("/Users/s1249052/PhD/cytof/vac69a/T_cells_only/fcs")
 
 md <- read.csv("meta_data.csv", header=T) 
@@ -55,23 +61,25 @@ vac69a <- read.flowSet(md$file_name)
 
 ### define panel using first fcs file in flowset, then clean up
 
-panel <- data.frame("fcs_colname"=colnames(vac69a[[1]]), "antigen"=c("Time", markernames(vac69a[[1]])))
-panel$antigen <- gsub("_", "", panel$antigen)
+# panel <- data.frame("fcs_colname"=colnames(vac69a[[1]]), "antigen"=c("Time", markernames(vac69a[[1]])))
+# panel$antigen <- gsub("_", "", panel$antigen)
+# 
+# panel$antigen <- ifelse(nchar(panel$antigen)>6,
+#                         substr(panel$antigen, 6, nchar(panel$antigen)),
+#                         panel$antigen
+# )
+# 
+# panel$antigen[3] <- "CD45"
+# 
+# panel$antigen <- gsub("-", "", panel$antigen)
+# panel$antigen <- gsub(".", "", panel$antigen, fixed=T)
+# 
+# write.csv(panel, "VAC69_PANEL.CSV")
+panel <- read.csv("VAC69_PANEL.CSV", header=T)
+####
 
-panel$antigen <- ifelse(nchar(panel$antigen)>6,
-                        substr(panel$antigen, 6, nchar(panel$antigen)),
-                        panel$antigen
-)
-
-panel$antigen[3] <- "CD45"
-
-panel$antigen <- gsub("-", "", panel$antigen)
-panel$antigen <- gsub(".", "", panel$antigen, fixed=T)
-
-
-
-
-### construct daFrame
+### CATALYST ####
+#construct daFrame #
 ## md has to have particular properties: file_name=NAMED LIST (chr), ID and everything else in factors
 daf <- daFrame(vac69a, panel, md, md_cols =
                  list(file = "file_name", id = "sample_id", factors = c("timepoint", "batch", "volunteer")))
@@ -121,43 +129,66 @@ marker_levels <- c("CD4",
 t_cell_channels <- marker_levels
 
 
-
+### metaclustering merged central memory and naive cells...
 daf100 <- cluster(daf, cols_to_use = t_cell_channels, xdim = 10, ydim = 10, maxK = 15, seed = 1234)
-daf196 <- cluster(daf, cols_to_use = t_cell_channels, xdim = 14, ydim = 14, maxK = 25, seed = 1234)
 
-daf100_delta <- metadata(daf100)$delta_area
-daf196_delta <- metadata(daf196)$delta_area
-daf100_delta
-daf196_delta
+# daf100_delta <- metadata(daf100)$delta_area
 
-end_time <- Sys.time()
-
-duration = end_time - start_time
-#duration = ~5min
-
+#### get rid of weird super positive events ####
+cluster_ids <- cluster_ids(daf100, "meta15")
+clusters <- "15"
+daf100 <- daf100[cluster_ids %!in% clusters,]####
 
 plotClusterHeatmap(daf100,
-                   hm2 = 'abundances',
+                   hm2 = 'TIM3',
                    #k = "meta15",
-                   k = "som100", 
-                   m = "meta12",
-                   cluster_anno = F                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               ,
-                   draw_freqs = F)
-                   #split_by='timepoint')
+                   k = "meta15", 
+                   #m = "meta15",
+                   cluster_anno = T                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               ,
+                   draw_freqs = T,
+                   scale=T)
+                   
+#split_by='timepoint')
                
 
-plotAbundances(daf100, k = "meta195", by = "cluster_id", shape="volunteer")
-FDR_cutoff <- 0.05
+plotClusterExprs(daf100, k = "meta15", markers = "state")
+plotAbundances(daf100, k = "meta15", by = "cluster_id", shape="volunteer")
 
-### differential analysis using pairwise comparisons implemented in diffcyt
+merging_table1 <- data.frame(read_excel("flo_cluster_merging1.xlsx"))
+merging_table1$original_cluster <- as.character(merging_table1$original_cluster)
+merging_table1$new_cluster <- factor(merging_table1$new_cluster, levels = c("CD4+_Naive",
+                                                                            "Treg",
+                                                                            "CD4+_Effectors_CD57-ICOS+",
+                                                                            "CD4+_Effectors_CD57intGZBint",
+                                                                            "CD4+_Effectors_CD57+GZB+", 
+                                                                            "CD4+_Effectors_CD27-HLADR+",
+                                                                            "CD8+_Naive",
+                                                                            "CD8+_EM_PD1+",
+                                                                            "CD8+EffectorsCD57+Tbet+",
+                                                                            "CD8+_Effectors_CD57-CD45RA+CD45RO-",
+                                                                            "MAIT",
+                                                                            "gd_CD161-CD28-",
+                                                                            "gd_CD161+_CD28+",
+                                                                            "DN",
+                                                                            "trash"
+                                                                            ))
+
+
+daf100 <- mergeClusters(daf100, k = "meta15", table = merging_table1, id = "merging1")
+
+plotDR(daf100, "UMAP", color_by="CD45RO")
+####
+
+
+### differential analysis using pairwise comparisons implemented in diffcyt ####
 ei <- metadata(daf100)$experiment_info
 
 da_formula1 <- createFormula(ei, cols_fixed = "timepoint",
                              cols_random = c("sample_id"))
 
 (da_formula2 <- createFormula(ei,
-                              cols_fixed = "timepoint", 
-                              cols_random = c("sample_id", "volunteer")))
+                              cols_fixed = c("timepoint", "volunteer"), 
+                              cols_random = "sample_id"))
 
 
 #try som100; run colnames(metadata(daf100)$cluster_codes) to see everything
@@ -167,7 +198,8 @@ da_formula1 <- createFormula(ei, cols_fixed = "timepoint",
 levels(ei$timepoint)
 # [1] "Baseline" "C10"      "DoD"      "T6"   
 
-contrast_baseline <- createContrast(c(, rep(0, 3)))
+# try putting baserline level as -1 rather than 0...
+contrast_baseline <- createContrast(c(1, rep(0, 3)))
 contrast_c10 <- createContrast(c(0,1,0,0))
 contrast_dod <- createContrast(c(0,0,1,0))
 contrast_t6 <- createContrast(c(rep(0, 3), 1))
@@ -179,7 +211,7 @@ da_baseline <- diffcyt(daf100,
                  contrast = contrast_baseline,
                  analysis_type = "DA",
                  method_DA = "diffcyt-DA-GLMM",
-                 clustering_to_use = "meta15",
+                 clustering_to_use = "som100",
                  verbose = F)
 
 
@@ -189,7 +221,7 @@ da_c10 <- diffcyt(daf100,
                  contrast = contrast_c10,
                  analysis_type = "DA",
                  method_DA = "diffcyt-DA-GLMM",
-                 clustering_to_use = "meta15",
+                 clustering_to_use = "som100",
                  verbose = F)
 
 da_dod <- diffcyt(daf100,
@@ -198,7 +230,7 @@ da_dod <- diffcyt(daf100,
                  contrast = contrast_dod,
                  analysis_type = "DA",
                  method_DA = "diffcyt-DA-GLMM",
-                 clustering_to_use = "meta15",
+                 clustering_to_use = "som100",
                  verbose = F)
 
 
@@ -208,7 +240,7 @@ da_t6 <- diffcyt(daf100,
                    contrast = contrast_t6,
                    analysis_type = "DA",
                    method_DA = "diffcyt-DA-GLMM",
-                   clustering_to_use = "meta15",
+                   clustering_to_use = "som100",
                    verbose = F)
 
 
@@ -247,7 +279,7 @@ da_t6_vol <- diffcyt(daf100,
                  verbose = F)
 
 
-
+FDR_cutoff <- 0.05
 
 plotDiffHeatmap(daf100, da_baseline, th = FDR_cutoff, normalize = TRUE, hm1 = T)
 plotDiffHeatmap(daf100, da_c10, th = FDR_cutoff, normalize = TRUE, hm1 = T)
@@ -255,7 +287,96 @@ plotDiffHeatmap(daf100, da_dod, th = FDR_cutoff, normalize = TRUE, hm1 = T)
 plotDiffHeatmap(daf100, da_t6, th = FDR_cutoff, normalize = TRUE, hm1 = T)
 
 
-plotDiffHeatmap(daf100, da_baseline_vol, th = FDR_cutoff, normalize = TRUE, hm1 = T)
+
+#### metacluster
+table(rowData(da_baseline$res)$p_adj < FDR_cutoff)
+# FALSE  TRUE 
+# 1    14 
+table(rowData(da_c10$res)$p_adj < FDR_cutoff)
+# FALSE 
+# 15 
+table(rowData(da_dod$res)$p_adj < FDR_cutoff)
+# FALSE FALSE  TRUE 
+# 15     94     6
+table(rowData(da_t6$res)$p_adj < FDR_cutoff)
+# FALSE  TRUE FALSE  TRUE 
+# 13     2     88    12
+
+####
+
+# export data ####
+
+### wrapper function extracClusters doesn't work somehow? using the source code for it's easy enough to
+### replicate and it seems to work just fin
+
+cluster_ids <- cluster_ids(daf100, "meta15")
+clusters <- c("2", "14")
+sig_t6_daf <- daf100[cluster_ids %in% clusters,]
+
+
+#### reclustering on t6 upregulated cells ####
+sig_t6_daf <- runDR(sig_t6_daf, "UMAP", rows_to_use = 2000)
+
+bcl2_plot <- plotDR(sig_t6_daf, "UMAP", color_by="BCL2", facet="timepoint")+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.position = "none", 
+        axis.title = element_blank())+
+  sc
+
+cd38_plot <- plotDR(sig_t6_daf, "UMAP", color_by="CD38", facet="timepoint")+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.position = "none", 
+        axis.title = element_blank())+
+  sc
+
+cluster_plot <- plotDR(sig_t6_daf, "UMAP", color_by="som100", facet="timepoint")+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.position = "none", 
+        axis.title = element_blank())
+
+plotAbundances(sig_t6_daf, k = "som100", by = "cluster_id", shape="volunteer")
+
+plotClusterHeatmap(sig_t6_daf,
+                   hm2 = 'abundances',
+                   #k = "meta15",
+                   k = "som100", 
+                   m = "meta15",
+                   cluster_anno = T                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               ,
+                   draw_freqs = F)
+#split_by='timepoint')
+
+
+panel <- data.frame("fcs_colname"=colnames(sig_t6[[1]]), "antigen"=c("Time", markernames(sig_t6[[1]])))
+panel$antigen <- gsub("_", "", panel$antigen)
+
+panel$antigen <- ifelse(nchar(panel$antigen)>6,
+                        substr(panel$antigen, 6, nchar(panel$antigen)),
+                        panel$antigen
+)
+
+panel$antigen[3] <- "CD45"
+
+panel$antigen <- gsub("-", "", panel$antigen)
+panel$antigen <- gsub(".", "", panel$antigen, fixed=T)
+
+
+
+
+
+
+
+
+
+
+plotMedExprs(daf100, k = "meta15", facet = "antigen", shape_by = "volunteer", group_by="timepoint")
+
+
+
+
+dafplotDiffHeatmap(daf100, da_baseline_vol, th = FDR_cutoff, normalize = TRUE, hm1 = T)
 plotDiffHeatmap(daf100, da_c10_vol, th = FDR_cutoff, normalize = TRUE, hm1 = T)
 plotDiffHeatmap(daf100, da_dod_vol, th = FDR_cutoff, normalize = TRUE, hm1 = T, top_n = 55)
 plotDiffHeatmap(daf100, da_t6_vol, th = FDR_cutoff, normalize = TRUE, hm1 = T, top_n = 55)
