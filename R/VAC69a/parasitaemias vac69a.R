88000f66d04c126c377431090402552df24c4d6e
florian-bach <s1249052@sms.ed.ac.uk>
2020-04-17 14:44:42 +0100

    fixed filepaths, added parasitaemia theme & discrete x axis details for consistency between plots

--- R/VAC69a/parasitaemias vac69a.R
+++ R/VAC69a/parasitaemias vac69a.R
@@ -6,17 +6,15 @@ library(gtools)
 library(directlabels)
 
 #setwd("/Users/s1249052/PhD/oxford/vac69")
-setwd("~/PhD")
-data <- read.csv("vac69a_parasitaemia.csv", header=T)
+data <- read.csv("~/PhD/clinical_data/vac69a/parasitaemia/vac69a_parasitaemia.csv", header=T)
 
 # get rid of garbage timepoints that mess up graph
 
 parasitaemias <- gather(data, Timepoint, Genomes, c("C.1", colnames(data)[12:35]) )
 parasitaemias[,2:13] <- NULL
 
-parasitaemias$Timepoint <- gsub("C.1", "C-1", parasitaemias$Timepoint) 
+parasitaemias$Timepoint <- gsub("C.1", "Baseline", parasitaemias$Timepoint) 
 parasitaemias$Timepoint <- gsub("D5.5.1", "D5.5", parasitaemias$Timepoint) 
-parasitaemias$Timepoint <- gsub("D", "C+", parasitaemias$Timepoint) 
 
 
 
@@ -33,8 +31,19 @@ dods <- data.frame(vol=c('v02', 'v03', 'v05', 'v06', 'v07', 'v09'),
 dods$inter <- match(dods$dod, unique(parasitaemias$Timepoint))
 
 
+parasitaemia_theme <-  theme(legend.title = element_blank(),
+                             legend.text = element_text(size = 20) ,
+                             axis.line = element_line(colour = "black"),
+                             axis.text.x = element_text(angle = 60, hjust = 1, size=18, color="black"),
+                             axis.title.x = element_text(size=24, color="black"),
+                             axis.title.y = element_text(size=24, color="black"),
+                             axis.text.y = element_text(size=20, color="black"),
+                             legend.position = "top")
 
-ggplot(data=parasitaemias[!is.na(parasitaemias$Genomes),], aes(x=factor(Timepoint, levels = unique(mixedsort(parasitaemias$Timepoint))), y=Genomes, group=factor(Volunteer)))+
+
+
+
+vac69a_plot <- ggplot(data=parasitaemias[!is.na(parasitaemias$Genomes),], aes(x=factor(Timepoint, levels = unique(mixedsort(parasitaemias$Timepoint))), y=Genomes, group=factor(Volunteer)))+
   geom_point(aes(color=factor(Volunteer)), size=2.5)+
   geom_line(aes(color=factor(Volunteer)), size=2)+
   scale_color_manual(values=my_paired_palette)+
@@ -42,22 +51,14 @@ ggplot(data=parasitaemias[!is.na(parasitaemias$Genomes),], aes(x=factor(Timepoin
   xlab("Day of Infection")+
   ylab("Genome Copies / mL")+
   scale_y_continuous(trans="log10", limits=c(1, 27000), breaks=c(10, 100, 1000, 10000, 25000))+
-  theme(legend.title = element_blank(),
-        legend.text = element_text(size = 20) ,
-        axis.line = element_line(colour = "black"),
-        axis.text.x = element_text(angle = 60, hjust = 1, size=18, color="black"),
-        axis.title.x = element_text(size=24, color="black"),
-        axis.title.y = element_text(size=24, color="black"),
-        axis.text.y = element_text(size=20, color="black"))#+
-  geom_segment(data=dods, aes(x=inter, xend=inter, y = 0, yend=paras), colour=my_paired_palette, position=position_jitter(), inherit.aes=F)#+
-  geom_segment(data=dods, aes(x=0, xend=inter, y = paras, yend=paras), colour=my_paired_palette, inherit.aes=F)
-  
-              
- ggsave ("/Users/s1249052/PhD/oxford/vac69/parasitaemias_vac69.pdf", height = 8, width=10)
- ggsave ("/Users/s1249052/PhD/oxford/vac69/parasitaemias_vac69.png", height = 8, width=10)
- 
+  scale_x_discrete(limits=c("Baseline", paste("D", seq(6, 17), sep='')))+
+  parasitaemia_theme
 
-ggplot(dods, aes(x=vol, y=paras, fill=vol))+
+#ggsave ("/Users/s1249052/PhD/oxford/vac69/parasitaemias_vac69.png", height = 8, width=10)
+ggsave("~/PhD/clinical_data/vac69a/figures/vac69a_parasitaemia.png", vac69a_plot, height = 8, width=10)
+
+
+dods_plot <- ggplot(dods, aes(x=vol, y=paras, fill=vol))+
   geom_bar(stat='identity')+
   theme_bw()+
   scale_fill_manual(values=my_paired_palette)+
@@ -71,6 +72,7 @@ ggplot(dods, aes(x=vol, y=paras, fill=vol))+
         axis.title.x = element_blank(),
         axis.text.y = element_text(size=20, color="black"))
  
-ggsave ("/Users/s1249052/PhD/oxford/vac69/dod_parasitaemias_vac69.pdf", height = 8, width=10)
+#ggsave ("/Users/s1249052/PhD/oxford/vac69/dod_parasitaemias_vac69.pdf", height = 8, width=10)
+ggsave("~/PhD/clinical_data/vac69a/figures/vac69a_parasitaemia_at_dod.png", dods_plot, height = 8, width=10)
 
 
